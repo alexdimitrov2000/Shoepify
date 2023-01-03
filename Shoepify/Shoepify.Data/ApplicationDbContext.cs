04725f6ce6ec2271b25c0a06cc0b2af0f036a8b2
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-01-03 21:43:55 +0200

    Added a model and a DbContext.

---
+++ Shoepify/Shoepify.Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shoepify.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoepify.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Shoe> Shoes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Shoepify/Shoepify.Domain/Shoe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoepify.Domain
+{
+    public class Shoe
+    {
+        public int Id { get; set; }
+
+        public string? Brand { get; set; }
+
+        public string? Model { get; set; }
+
+        public int Size { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
