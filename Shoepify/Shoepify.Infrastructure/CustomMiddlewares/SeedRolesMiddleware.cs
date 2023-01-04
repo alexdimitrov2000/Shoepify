c4fc074e53eae88466a8f566dedde783e3bb5a3c
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-01-04 20:09:48 +0200

    Added a custom middleware for seeding user roles in the database.

---
+++ Shoepify/Shoepify.Infrastructure/CustomMiddlewares/SeedRolesMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoepify.Infrastructure.CustomMiddlewares
+{
+    public class SeedRolesMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public SeedRolesMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, RoleManager<IdentityRole> roleManager)
+        {
+            if(!roleManager.Roles.Any())
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+                await roleManager.CreateAsync(new IdentityRole("User"));
+            }
+
+            await this.next(httpContext);
+        }
+    }
+}
---
+++ Shoepify/Shoepify.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Shoepify.Infrastructure.CustomMiddlewares;
+
+namespace Shoepify.Infrastructure.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder SeedRoles(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SeedRolesMiddleware>();
+        }
+    }
+}
