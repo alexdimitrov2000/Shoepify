3fa5cb22ffa8905c5792b78412b2b70e536ad4c5
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-01-08 15:49:28 +0200

    Added a cart Index view.

---
+++ Shoepify/Shoepify.Web/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shoepify.Data;
+using Shoepify.Domain;
+using Shoepify.Infrastructure.Extensions;
+using Shoepify.Web.Models.Cart;
+
+namespace Shoepify.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ShoepifyContext context;
+
+        public CartController(ShoepifyContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+            CartDetailsViewModel cartVM = new CartDetailsViewModel()
+            {
+                CartItems= cart,
+                TotalPrice = cart.Sum(i => i.Price * i.Quantity)
+            };
+
+            return View(cartVM);
+        }
+    }
+}
---
+++ Shoepify/Shoepify.Web/Models/Cart/CartDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Shoepify.Domain;
+
+namespace Shoepify.Web.Models.Cart
+{
+    public class CartDetailsViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
