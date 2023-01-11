b9a220bf979560773968c89030c75fba63679f3b
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-01-11 20:57:11 +0200

    Implemented Request Query extension method for filtering list of shoes by gender and category.

---
+++ Shoepify/Shoepify.Infrastructure/Extensions/RequestQueryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Shoepify.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoepify.Infrastructure.Extensions
+{
+    public static class RequestQueryExtensions
+    {
+        public static List<Shoe> GetFilteredShoes(this IQueryCollection query, List<Shoe> collection)
+        {
+            StringValues genderParam = query["gender"];
+            StringValues categoryParam = query["category"];
+
+            if (genderParam.Count != 0 && genderParam[0] != null)
+            {
+                collection = collection.Where(x => x.Gender.ToString().ToLower() == genderParam[0]?.ToLower()).ToList();
+            }
+            if (categoryParam.Count != 0 && categoryParam[0] != null)
+            {
+                collection = collection.Where(x => x.Category?.Name?.ToLower() == categoryParam[0]?.ToLower()).ToList();
+            }
+
+            return collection;
+        }
+    }
+}
