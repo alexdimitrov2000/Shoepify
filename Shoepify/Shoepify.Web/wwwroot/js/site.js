3e7129bb0b0e05fed16da734503b669e2c3bbf3b
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-01-12 03:31:41 +0200

    Added a script for preventing name field, in the filter form, to send value to the query string if it is not set.

--- Shoepify/Shoepify.Web/wwwroot/js/site.js
+++ Shoepify/Shoepify.Web/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#filter").submit(function () {
+        if ($("#name-field").val() == "") {
+            $("#name-field").remove();
+        }
+    });
+});
