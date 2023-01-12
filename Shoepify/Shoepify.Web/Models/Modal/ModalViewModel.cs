b18d6b145088de0f1f1aa4cf30fabc8a29084839
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-01-12 20:12:23 +0200

    Extracted the confirmation modal into a separate view component.

---
+++ Shoepify/Shoepify.Web/Models/Modal/ModalViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shoepify.Web.Models.Modal
+{
+    public class ModalViewModel
+    {
+        public int ShoeId { get; set; }
+
+        public string? ProductName { get; set; }
+    }
+}
---
+++ Shoepify/Shoepify.Web/ViewComponents/ModalViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shoepify.Web.Models.Modal;
+
+namespace Shoepify.Web.ViewComponents
+{
+    public class ModalViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int shoeId, string brand, string model)
+        {
+            var modalVM = new ModalViewModel()
+            {
+                ShoeId = shoeId,
+                ProductName = $"{brand} {model}"
+            };
+
+            return View(modalVM);
+        }
+    }
+}
